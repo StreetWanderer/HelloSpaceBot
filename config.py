66065be166eb0fa8c174a7bb21b2760f4a576d78
WanderNico <nicolas@streetwanderer.com>
2015-05-04 18:39:49 -0400

    improve config readability

--- config.py
+++ config.py
@@ -1,5 +1,15 @@
 OPUS_API_BASE_URL = 'http://pds-rings-tools.seti.org/opus/api/'
-PLANET_ID_TABLE = {'MER': 'Mercury', 'VEN': 'Venus', 'EAR': 'Earth', 'MAR': 'Mars', 'JUP': 'Jupiter', 'SAT': 'Saturn', 'URA': 'Uranus', 'PLU':'Pluto'}
+
+PLANET_ID_TABLE = {'MER': 'Mercury', 
+				   'VEN': 'Venus', 
+				   'EAR': 'Earth', 
+				   'MAR': 'Mars', 
+				   'JUP': 'Jupiter', 
+				   'SAT': 'Saturn', 
+				   'URA': 'Uranus', 
+				   'PLU':'Pluto'
+				   }
+
 CRAFT_LIST = [{'craft': 'Cassini', 'pages':1}, 
 			  {'craft':'Hubble', 'pages':1}, 
 			  {'craft':'Galileo', 'pages':1}, 
