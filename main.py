8522847cd360a65e57ccfb0bbcf42651681732ed
Streetwanderer <nicolas@streetwanderer.com>
2016-01-10 18:27:48 -0500

    Remove infinite loop

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
-import time
+#import time
 import core
 
 
 #frequencyInSec = 60.0 * 1.0 #every minute
-frequencyInSec = 60.0 * 30.0 #every 30 minutes
-postToTumblr = True
+#frequencyInSec = 60.0 * 30.0 #every 30 minutes
+#postToTumblr = True
 
 
 
-startTime = time.time()
-step = frequencyInSec - ((time.time() - startTime) % frequencyInSec)
+#startTime = time.time()
+#step = frequencyInSec - ((time.time() - startTime) % frequencyInSec)
 
-while True:
-	if step > 1:
+#while True:
+#	if step > 1:
   		#print "tick - " + time.strftime("%H:%M:%S")
-  		core.main(postToTumblr)
-  		step = frequencyInSec - ((time.time() - startTime) % frequencyInSec)
-  		time.sleep(step)
-  	else:
-  		print "Error in step calculation - step="+step
+#  		core.main(postToTumblr)
+#  		step = frequencyInSec - ((time.time() - startTime) % frequencyInSec)
+#  		time.sleep(step)
+#  	else:
+#  		print "Error in step calculation - step="+step
+
+core.main(True)
